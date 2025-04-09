5040740552c3cf41548e02fd56e9b0857a3b7a4f
riteshk-007 <codeshorts007@gmail.com>
2025-04-09 17:50:12 +0530

    Fix typo in referrepolicy attribute in Contact component for correct iframe behavior

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -152,7 +152,7 @@ const Contact = () => {
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26428.70295067084!2d74.81165658983033!3d34.10569688072556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e185aa289c138b%3A0xd68ebadabcaaac43!2sNaaz%20Kashmir!5e0!3m2!1sen!2sin!4v1744197829070!5m2!1sen!2sin"
                 width="400"
                 height="240"
-                referRepolicy="no-referrer-when-downgrade"
+                referrepolicy="no-referrer-when-downgrade"
               ></iframe>
             </div>
           </div>
